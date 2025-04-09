cfdf48c5c1e7068ea015eaa0ae85ec3500686211
Ryan <ryeean16@gmail.com>
2025-04-09 13:45:19 -0700

    navigation third draft

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,11 +2,35 @@ import styles from './Navigation.module.scss';
 import { Menu, User, Users, MessageSquare, Home } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Navigation() {
 
   const pathname = usePathname();
   const router = useRouter();
+
+  const [loading, setLoading] = useState(true)
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect (() => {
+    async function fetchProfile() {
+      try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (!user || !user.id) {
+          console.error("No user found in localStorage.");
+          return;
+        }
+        const response = await fetch (`http://127.0.0.1:8000/volunteer/${user.id}/`);
+        const data = await response.json();
+        setProfileData(data.volunteer);
+        setLoading(false)
+      } catch {
+        console.error("Error fetching profile data:", error);
+        setLoading(false)
+      }
+    }
+    fetchProfile();
+  }, [])
   
   return (
     <div className={styles.nav}>
@@ -32,7 +56,13 @@ export default function Navigation() {
 
       <div className={styles.profile}>
         <img src="/assets/VFJC.png" alt="Profile" />
-        <span className={styles.profileName}>Ryan Yee</span>
+        <span className={styles.profileName}>
+          {profileData ? (
+            <div>{profileData.first_name} {profileData.last_name}</div>
+          ) : (
+            <p>loading...</p>
+          )}
+        </span>
       </div>
     </div>
   );
