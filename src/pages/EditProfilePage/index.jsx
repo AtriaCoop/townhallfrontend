e95345d39c50a675ec27e25314d9e2eaed8627bf
Ryan <ryeean16@gmail.com>
2025-04-14 20:13:50 -0700

    added profile page and edit profile page

---
+++ src/pages/EditProfilePage/index.jsx
@@ -0,0 +1,54 @@
+import styles from '@/pages/EditProfilePage/EditProfilePage.module.scss'
+import Navigation from '@/components/Navigation/Navigation'
+
+export default function EditProfilePage() {
+    return (
+        <div className={styles.container}>
+            <Navigation />
+
+            <div className={styles.editProfileContainer}>
+                <h1>Account Settings</h1>
+                <img className={styles.profilePic} src="/assets/test.png" alt="Profile Image" />
+                <h3 className={styles.name}>Ryan Yee</h3>
+            </div>
+
+            <div className={styles.inputs}>
+                    <p>Email</p>
+                        <input type="text" placeholder='Enter email...'/>
+                    <p>Full Name</p>
+                        <input type="text" placeholder='Enter full name...'/>
+                    <p>Preferred Pronouns</p>
+                        <input type="text" placeholder='Share your pronouns (Optional)'/>
+                    <p>Title</p>
+                        <input type="text" placeholder='Enter title...'/>
+                    <p>Primary Organization</p>
+                        <input type="text" placeholder='What is the main organization you work for?'/>
+                    <p>Other Organizations</p>
+                        <input type="text" placeholder='Enter other organizations you are a part of...'/>
+                    <p>Other Networks</p>
+                        <input type="text" placeholder='List any coalitions or networks you are connected to other than the VFJC.'/>
+                    <p>About Me</p>
+                        <input type="text" placeholder='Tell us about yourself!'/>
+                    <p>Skills & Interests</p>
+                        <input type="text" placeholder='Enter any skills & interests that may benfeit the coalition.'/>
+                    <p>Profile Image</p>
+                        <div className={styles.profileImageInput}>
+                            <img src="/assets/test.png" alt="Profile Image" />
+                        </div>
+                            <input type="file" accept="image/*" style={{ display: 'none' }}/>
+                    <p>Profile Header</p>
+                        <div className={styles.profileHeaderInput}>
+                            <img src="/assets/test.png" alt="Profile Header" />
+                        </div>
+                            <input type="file" accept="image/*" style={{ display: 'none' }}/>
+
+                    <button className={styles.saveButton}>SAVE</button>
+                </div>
+
+                <div className={styles.deleteButton}>
+                    <p>DELETE ACCOUNT</p>
+                </div>
+
+        </div>
+    )
+}
---
+++ src/pages/ProfilePage/index.jsx
@@ -0,0 +1,37 @@
+import styles from '@/pages/ProfilePage/ProfilePage.module.scss'
+import Navigation from '@/components/Navigation/Navigation'
+import { useRouter } from 'next/router';
+
+export default function ProfilePage() {
+
+    const router = useRouter();
+
+    return (
+        <div className={styles.container}>
+            <Navigation />
+
+            <div className={styles.profileContainer}>
+                <img className={styles.profilePic} src="/assets/test.png" alt="Profile Image" />
+                <span className={styles.name}>Ryan Yee</span>
+                <span className={styles.dateJoined}>Date Joined: 2 weeks ago</span>
+                <div className={styles.profileInfo}>
+                    <span>Title: Volunteer</span>
+                    <span>spanrimary Organization: Atria</span>
+                    <span>Other Organizations: other</span>
+                    <span>Other Networks: other</span>
+                    <span>About Me: other</span>
+                    <span>My Skills & Interests: other</span>
+                </div>
+                <button onClick={() => router.push('/EditProfilePage')} className={styles.editButton}>EDIT MY PROFILE</button>
+                
+                <div className={styles.signoutContainer}>
+                    <p>Time to go?</p>
+                    <button onClick={() => router.push('/')} className={styles.signoutButton}>
+                        SIGN OUT
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
