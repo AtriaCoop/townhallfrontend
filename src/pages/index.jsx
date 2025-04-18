413bb4fb6208dc42254e7a500cf1ab3d5bdd817c
Ryan <ryeean16@gmail.com>
2025-04-17 17:27:19 -0700

    Hid landing page for testing in production

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import Landing from "@/pages/LandingPage/index";
+import HomePage from "./HomePage";
 
 export default function Home() {
     return (
-      <Landing />
+      < HomePage />
     )
   }
   
