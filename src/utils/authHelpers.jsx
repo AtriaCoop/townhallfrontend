8e76cb1f23c4ae1d16fc5b4f7189b149d1be6d9c
Ryan <ryeean16@gmail.com>
2025-05-14 22:34:50 -0700

    exported getCookie

--- src/utils/authHelpers.jsx
+++ src/utils/authHelpers.jsx
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE;
 
-const getCookie = (name) => {
+export const getCookie = (name) => {
     const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
     return match ? decodeURIComponent(match[2]) : null;
   };  
