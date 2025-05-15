6353517760f94ae9880c01d4906681fcc67b544c
Ryan <ryeean16@gmail.com>
2025-05-14 22:21:13 -0700

    update

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -48,11 +48,6 @@ export default function LandingPage() {
       }, 1000);
     }
   };
-  
-  function getCookie(name) {
-    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
-    return match ? decodeURIComponent(match[2]) : null;
-  }
 
   // SIGN IN
   const handleLogIn = async (event) => {
