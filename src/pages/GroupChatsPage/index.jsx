0ddfcb886f0959ef4e1eecbd81a29489841ab655
kprimora <kadenpt@gmail.com>
2025-05-31 17:57:25 -0700

    afr-121-replace-send-icon

--- src/pages/GroupChatsPage/index.jsx
+++ src/pages/GroupChatsPage/index.jsx
@@ -64,7 +64,7 @@ export default function GroupChatsPage() {
                     placeholder="Enter message"
                     />
                     <button className={styles.sendButton}>
-                        <img src="/assets/send.svg" alt="Send" />
+                        <img src="/assets/send.png" alt="Send" />
                     </button>
                 </div>
             </div>
