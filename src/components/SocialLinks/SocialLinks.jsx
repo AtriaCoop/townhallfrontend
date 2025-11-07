3ef8cc0826d12c5832aa03dd53c8c7172b1211ac
Mona Dasig <monalizadasig@gmail.com>
2025-11-07 14:06:54 -0800

    add SocialLinks component
    - displaying social media icons with links

---
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -0,0 +1,50 @@
+import {
+  FaLinkedin,
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaSquareFacebook,
+} from "react-icons/fa6";
+import styles from "./SocialLinks.module.scss";
+
+export default function SocialLinks({ socialLinks }) {
+  const links = [
+    {
+      url: socialLinks.linkedin_url,
+      icon: <FaLinkedin />,
+      name: "LinkedIn",
+    },
+    {
+      url: socialLinks.x_url,
+      icon: <FaSquareXTwitter />,
+      name: "X",
+    },
+    {
+      url: socialLinks.facebook_url,
+      icon: <FaSquareFacebook />,
+      name: "Facebook",
+    },
+    {
+      url: socialLinks.instagram_url,
+      icon: <FaSquareInstagram />,
+      name: "Instagram",
+    },
+  ];
+
+  return (
+    <div className={styles.socialLinks}>
+      {links
+        .filter((link) => !!link.url)
+        .map(({ url, icon, name }) => (
+          <a
+            key={name}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            {icon}
+          </a>
+        ))}
+    </div>
+  );
+}
