f255fd9e25d498de27189b59416627924a5613fe
Mona Dasig <monalizadasig@gmail.com>
2026-02-24 14:02:58 -0800

    feat: add constants for languages

---
+++ src/constants/languages.js
@@ -0,0 +1,20 @@
+export const LANGUAGES = [
+  {
+    code: "en-US",
+    flag: "🇺🇸",
+    name: "English",
+    nativeName: "English (United States)",
+  },
+  {
+    code: "fr",
+    flag: "🇫🇷",
+    name: "French",
+    nativeName: "Français",
+  },
+  {
+    code: "es",
+    flag: "🇪🇸",
+    name: "Spanish",
+    nativeName: "Español",
+  },
+];
